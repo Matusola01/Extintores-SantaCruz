ebc76159ac54408d8b9bf664804def66591cf0e0
Mateo <msolat01@gmail.com>
2022-09-28 16:57:28 -0300

    changes

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+require('./db.js');
 
 var app = express();
-var port = process.env.PORT || 3525;
+var port = process.env.PORT || 3000;
 
 // Convierte una petición recibida (POST-GET...) a objeto JSON
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,5 +18,5 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
 	console.log(`Server running in http://localhost:${port}`);
 	console.log('Defined routes:');
-	console.log('	[GET] http://localhost:3525/');
+	console.log('	[GET] http://localhost:3000/');
 });
